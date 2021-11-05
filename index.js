1c9e0858957516bd4ff286fda5cfd877f4b66de4
Breanne <BreMarotta@gmail.com>
2021-11-05 11:42:50 -0400

    commit tree 4879c8109d3975eb68c61bf15e5215be343cbc21

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+    customerName = 'BOB';
+    return customerName;
+}
+
+function setBestCustomer(){
+    bestCustomer= "not bob";
+}
+
+function overwriteBestCustomer(){
+    bestCustomer= "maybe bob";
+}
+
+const leastFavoriteCustomer = "Stacy";
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "Tim";
+}
